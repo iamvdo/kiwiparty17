756e326898d380bb6f04b531156c1b345cfe782d
Vincent De Oliveira <vincent@iamvdo.me>
2017-10-25 10:24:02 +0200

    Add notes-server

--- plugin/notes-server/client.js
+++ plugin/notes-server/client.js
@@ -6,7 +6,7 @@
 	var socket = io.connect( window.location.origin ),
 		socketId = Math.random().toString().slice( 2 );
 
-	console.log( 'View slide notes at ' + window.location.origin + '/notes/' + socketId );
+	console.log( 'View slide notes at ' + window.location.origin + '/notes/' + socketId + '?receiver');
 
 	window.open( window.location.origin + '/notes/' + socketId, 'notes-' + socketId );
 
@@ -42,22 +42,22 @@
 
 	// When a new notes window connects, post our current state
 	socket.on( 'new-subscriber', function( data ) {
-		post();
+		//post();
 	} );
 
 	// When the state changes from inside of the speaker view
 	socket.on( 'statechanged-speaker', function( data ) {
-		Reveal.setState( data.state );
+		//Reveal.setState( data.state );
 	} );
 
 	// Monitor events that trigger a change in state
 	Reveal.addEventListener( 'slidechanged', post );
-	Reveal.addEventListener( 'fragmentshown', post );
-	Reveal.addEventListener( 'fragmenthidden', post );
-	Reveal.addEventListener( 'overviewhidden', post );
-	Reveal.addEventListener( 'overviewshown', post );
-	Reveal.addEventListener( 'paused', post );
-	Reveal.addEventListener( 'resumed', post );
+	//Reveal.addEventListener( 'fragmentshown', post );
+	//Reveal.addEventListener( 'fragmenthidden', post );
+	//Reveal.addEventListener( 'overviewhidden', post );
+	//Reveal.addEventListener( 'overviewshown', post );
+	//Reveal.addEventListener( 'paused', post );
+	//Reveal.addEventListener( 'resumed', post );
 
 	// Post the initial state
 	post();
